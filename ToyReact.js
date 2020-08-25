e8fbb2073f88a0384df286f06d0cec3efbdd33c3
xdSi <3450236968@qq.com>
2020-08-25 10:53:00 +0800

    feat: 完成虚拟DOM树的渲染

--- ToyReact.js
+++ ToyReact.js
@@ -1,47 +1,21 @@
 const RENDER_TO_DOM = Symbol('render to dom')
 
-class ElementWrapper {
-    constructor(type) {
-      this.root = document.createElement(type)
-    }
-
-    setAttribute(name, value) {
-      this.root.setAttribute(name, value)
-    }
-
-    appendChild(component) {
-      const range = document.createRange();
-      range.setStart(this.root, this.root.childNodes.length);
-      range.setEnd(this.root, this.root.childNodes.length);
-      component[RENDER_TO_DOM](range);
-    }
-
-    [RENDER_TO_DOM](range) {
-      range.deleteContents();
-      range.insertNode(this.root);
-    }
-
-}
-
-class TextNodeWrapper {
-  constructor(content) {
-    this.root = document.createTextNode(content);
-  }
-
-  [RENDER_TO_DOM](range) {
-    range.deleteContents();
-    range.insertNode(this.root);
-  }
-}
-
 export class Component {
 
   constructor(props) {
     this.props = Object.create(null);
     this._root = null;
+    this._range = null;
     this.children = []
   }
 
+  get vdom() {
+    return this.render().vdom
+  }
+
+  get vchildren() {
+    return this.children.map(child => child.vdom)
+  }
 
   setAttribute(name, value) {
       this.props[name] = value; 
@@ -52,10 +26,99 @@ export class Component {
   }
 
   [RENDER_TO_DOM](range) {
+    this._range = range;
     this.render()[RENDER_TO_DOM](range);
   }
 
+
+  rerender() {
+      this._range.deleteContents();
+      this[RENDER_TO_DOM](this._range) 
+  }
+
+  setState(newState) {
+    if(this.state === null && typeof this.state !== 'object') {
+      this.state = newState;
+      this.rerender();
+      return;
+    }
+
+    let merge = (oldState, newState) => {
+        for (const key in newState) {
+          if(oldState[key] === null || typeof oldState[key] !== 'object') {
+            oldState[key] = newState[key]
+          } else {
+            merge(oldState[key], newState[key]);
+          }
+        }
+    }
+    merge(this.state, newState);
+    this.rerender();
+  }
+
 }
+class ElementWrapper extends Component {
+    constructor(type) {
+      super(type);
+      this.type = type;
+    }
+
+    get vdom() {
+      return this;
+    }
+
+    [RENDER_TO_DOM](range) {
+      range.deleteContents();
+
+      let root = document.createElement(this.type);
+
+      for (const name in this.props) {
+        let value = this.props[name];
+        if (name.match(/^on([\s\S]+)/)) {
+          root.addEventListener(RegExp.$1.replace(/^[\s\S]/, s => s.toLowerCase()), value)
+        }
+        if (name === 'className') {
+          root.setAttribute('class', value)
+        } else {
+          root.setAttribute(name, value)
+        }
+      }
+
+      if(!this.vchildren)
+      this.vchildren = this.children.map(item => item.vdom);
+    
+      for (const child of this.vchildren) {
+          const childRange = document.createRange();
+          childRange.setStart(root, root.childNodes.length);
+          childRange.setEnd(root, root.childNodes.length);
+          child[RENDER_TO_DOM](childRange);
+      }
+
+      range.insertNode(root);
+    }
+
+  
+}
+
+class TextNodeWrapper extends Component {
+  constructor(content) {
+    super(content);
+    this.root = document.createTextNode(content);
+    this.content = content;
+    this.type = "#text";
+  }
+
+  get vdom() {
+    return this;
+  }
+
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root);
+  }
+}
+
+
 
 export const ToyReact = {
   createElement(type, attributes, ...children) {
@@ -75,6 +138,9 @@ export const ToyReact = {
         if(typeof child === 'string') {
           child = new TextNodeWrapper(child)
         }
+        if(!child) {
+          return;
+        }
         if(typeof child === 'object' && child instanceof Array) {
           insertChildren(child);
           return;
@@ -82,6 +148,7 @@ export const ToyReact = {
         element.appendChild(child)
       }
     }
+
     insertChildren(children);
     
     return element;
